75c85ab920926f2b18ae518f748d684577a424ad
Paul Ryan <g5muzu8u@duck.com>
2023-09-29 17:10:03 +1000

    always preview in cam-preview...

--- cam-preview.py
+++ cam-preview.py
@@ -8,7 +8,6 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--mode', help='the display mode', type=int, default=1)
     parser.add_argument('-v', '--video', help='video use case', action='store_true')
-    parser.add_argument('-p', '--preview', help='enable preview', action='store_true')
     parser.add_argument('--vflip', help='flip the image vertically', action='store_true')
     parser.add_argument('--hflip', help='flip the image horizontally', action='store_true')
     args = parser.parse_args()
@@ -16,7 +15,7 @@ def main():
     pipe = hqlib.camera(
         mode=args.mode, 
         video=args.video, 
-        preview=args.preview, 
+        preview=True, 
         vflip=args.vflip,
         hflip=args.hflip
     )
